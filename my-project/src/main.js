606d20d54d5a2c259b03d36dbd266274a8b6e4d7
yohannabb <yohannab2237@gmail.com>
2022-09-14 00:37:31 -0700

    first commit

---
+++ my-project/src/main.js
@@ -0,0 +1,7 @@
+import { createApp } from 'vue'
+import router from './router'
+import './style.css'
+import './index.css'
+import App from './App.vue'
+
+createApp(App).mount('#app')
---
+++ my-project/src/router/index.js
@@ -0,0 +1,24 @@
+import { createWebHistory, createRouter } from "vue-router";
+import Home from '../views/Home.vue';
+
+
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    component: Home,
+  },
+  {
+    path: "/about",
+    name: "About",
+    component:()=>import('../views/About.vue'),
+  },
+
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;
